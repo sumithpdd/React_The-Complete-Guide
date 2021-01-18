20111af8b4e113f71de6d207045ace3b03e4c0a2
sumith damodaran <sumithpd@gmail.com>
2021-01-18 23:45:21 +0100

    state and props

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -7,21 +7,26 @@ class App extends Component {
 
   state = {
     persons: [
-      {
-        name: 'Sumith', age: 40
-      },
+      { name: 'Sumith', age: 40 },
       { name: 'Sammy boy', age: 42 }
 
     ]
 
   }
-
+  swithNameHandler = () => {
+    // DON'T do this: this.state.person[0].name ='Sumith Damodaran';
+    this.setState({
+      persons: [
+        { name: 'Sumith Damodaran', age: 42 },
+        { name: 'The Great Sammy boy', age: 43 }]
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, I am a React APP</h1>
         <p>This is really working!</p>
-        <button>Switch Nme</button>
+        <button onClick={this.swithNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>my hobbies : Music</Person>
       </div>
