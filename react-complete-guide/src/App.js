b5518e167b6dcc991f89c2000751b9a643828a61
sumith damodaran <sumithpd@gmail.com>
2021-01-19 00:21:42 +0100

    adding 2 way data binding

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
-import { useState } from 'react';
+import { Component, useState } from 'react';
 
-const App = props => {
+class App extends Component {
 
-  const [personsState, setPersonsState] = useState({
+  state = {
     persons: [
       { name: 'Sumith', age: 40 },
       { name: 'Sammy boy', age: 42 }
@@ -13,32 +13,42 @@ const App = props => {
     ],
     otherState: "Some value"
 
-  });
-  const switchNameHandler = () => {
+  };
+  switchNameHandler = (newName) => {
     // DON'T do this: this.state.person[0].name ='Sumith Damodaran';
-    setPersonsState({
+    this.setState({
       persons: [
-        { name: 'Sumith Damodaran', age: 42 },
-        { name: 'The Great Sammy boy', age: 43 }],
-      otherState: personsState.otherState
+        { name: newName, age: 42 },
+        { name: newName, age: 43 }],
+      otherState: this.state.otherState
     })
   }
-
+  nameChangedHandler = (event) => {
+    // DON'T do this: this.state.person[0].name ='Sumith Damodaran';
+    this.setState({
+      persons: [
+        { name: event.target.value, age: 42 },
+        { name: event.target.value, age: 43 }],
+      otherState: this.state.otherState
+    })
+  }
+render(){
   return (
     <div className="App">
       <h1>Hi, I am a React APP</h1>
       <p>This is really working!</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
+      <button onClick={()=>this.switchNameHandler('Sumith Damodaran!!')}>Switch Name</button>
       <Person 
-      name={personsState.persons[0].name} 
-      age={personsState.persons[0].age} />
-      <Person click={switchNameHandler}
-      name={personsState.persons[1].name} 
-      age={personsState.persons[1].age}>my hobbies : Music</Person>
+      name={this.state.persons[0].name} 
+      age={this.state.persons[0].age} changed={this.nameChangedHandler} />
+      <Person click={this.switchNameHandler.bind(this,'The Great Sammy boy')}
+      name={this.state.persons[1].name} 
+      age={this.state.persons[1].age}
+      changed={this.nameChangedHandler}>my hobbies : Music</Person>
     </div>
   );
 
-
+}
 }
 
 export default App;
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -6,6 +6,7 @@ const Person=(props) =>{
         <div>
     <p onClick={props.click}>I'm a {props.name} and i am {props.age} old!</p>
     <p>{props.children}</p>
+    <input type="text" onChange={props.changed} value={props.name}/>
     </div>)
 };
 export default Person;
