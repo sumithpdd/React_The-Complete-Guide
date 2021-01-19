a36288c26b996786099253e33195d25877476636
sumith damodaran <sumithpd@gmail.com>
2021-01-19 01:51:09 +0100

    render content conditionally

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,18 +3,20 @@ import './App.css';
 import Person from './Person/Person';
 import { Component, useState } from 'react';
 import UserInput from './UserInput/UserInput';
-import UserOutput from './UserOutput/UserOutput'; 
+import UserOutput from './UserOutput/UserOutput';
 class App extends Component {
 
   state = {
     persons: [
       { name: 'Sumith', age: 40 },
-      { name: 'Sammy boy', age: 42 }
+      { name: 'Sammy boy', age: 42 },
+      { name: 'Stephanie', age: 31 }
 
     ],
     otherState: "Some value",
- 
-    userName:'Sumith Damodaran'
+
+    userName: 'Sumith Damodaran',
+    showPersons: false
   };
   switchNameHandler = (newName) => {
     // DON'T do this: this.state.person[0].name ='Sumith Damodaran';
@@ -37,40 +39,50 @@ class App extends Component {
   userNameChangedHandler = (event) => {
     // DON'T do this: this.state.person[0].name ='Sumith Damodaran';
     this.setState({
-          userName: event.target.value
+      userName: event.target.value
     })
   }
-render(){
-  const style={
-    backgroundColor:'white',
-    font:'inherit',
-    border:'1px solid blue',
-    padding:'8px',
-    cursor:'pointer'
-  };
-  return (
-    <div className="App">
-      <h1>Hi, I am a React APP</h1>
-      <p>This is really working!</p>
-      <button style={style}
-      onClick={()=>this.switchNameHandler('Sumith Damodaran!!')}>Switch Name</button>
-      <Person 
-      name={this.state.persons[0].name} 
-      age={this.state.persons[0].age} changed={this.nameChangedHandler} />
-      <Person click={this.switchNameHandler.bind(this,'The Great Sammy boy')}
-      name={this.state.persons[1].name} 
-      age={this.state.persons[1].age}
-      changed={this.nameChangedHandler}>my hobbies : Music</Person>
-    <UserInput changed={this.userNameChangedHandler} value={this.state.userName}/>
-    <UserOutput userName={this.state.userName}/>
-    
-    <UserOutput userName="Sammy"/>
-    
-    <UserOutput userName="SBoy"/>
-    </div>
-  );
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  }
 
-}
+  render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+    return (
+      <div className="App">
+        <h1>Hi, I am a React APP</h1>
+        <p>This is really working!</p>
+        <button style={style}
+          onClick={() => this.togglePersonsHandler()}>Switch Name</button>
+        { this.state.showPersons ?
+          <div >
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age} changed={this.nameChangedHandler} />
+            <Person click={this.togglePersonsHandler.bind(this, 'The Great Sammy boy')}
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              changed={this.nameChangedHandler}>my hobbies : Music</Person>
+          </div> : null
+        }
+        <hr/>
+        <UserInput changed={this.userNameChangedHandler} value={this.state.userName} />
+        <UserOutput userName={this.state.userName} />
+
+        <UserOutput userName="Sammy" />
+
+        <UserOutput userName="SBoy" />
+      </div>
+    );
+
+  }
 }
 
 export default App;
