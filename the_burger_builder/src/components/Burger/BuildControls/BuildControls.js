33a7e4e8ea4afa2e5d9fb3b033abf40956ffe65a
sumith damodaran <sumithpd@gmail.com>
2021-01-24 22:42:35 +0100

    Remove and total Price

--- the_burger_builder/src/components/Burger/BuildControls/BuildControls.js
+++ the_burger_builder/src/components/Burger/BuildControls/BuildControls.js
@@ -10,10 +10,13 @@ const controls = [
 export default function buildControls(props) {
     return (
         <div className={classes.BuildControls}>
+            <p>Current Price :<strong>{props.price.toFixed(2)}</strong> </p>
             {controls.map(ctrl => (
                 <BuildControl key={ctrl.label}
                     label={ctrl.label}
-                    added={() => props.ingredientAdded(ctrl.type)} />
+                    added={() => props.ingredientAdded(ctrl.type)} 
+                    removed={()=>props.ingredientRemoved(ctrl.type)}
+                    disabled ={props.disabled[ctrl.type]}/>
             )
             )}
         </div>
