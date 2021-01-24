85ad17251a9a7f42362c4ed2713c20d76502e0fa
sumith damodaran <sumithpd@gmail.com>
2021-01-24 16:59:27 +0100

    08 real App with dynamic ingredients

--- the_burger_builder/src/components/Burger/Burger.js
+++ the_burger_builder/src/components/Burger/Burger.js
@@ -2,11 +2,18 @@ import React from 'react'
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 export default function Burger(props) {
+    const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+            return [...Array(props.ingredients[igKey])]
+            .map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            })
+        }
+    )
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     )
