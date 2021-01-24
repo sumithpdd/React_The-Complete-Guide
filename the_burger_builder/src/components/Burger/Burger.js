c8899c76ffaf38e047b6cf0c00f29c00aa1319c8
sumith damodaran <sumithpd@gmail.com>
2021-01-24 21:18:36 +0100

    addIngredientHandler

--- the_burger_builder/src/components/Burger/Burger.js
+++ the_burger_builder/src/components/Burger/Burger.js
@@ -2,14 +2,19 @@ import React from 'react'
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 export default function Burger(props) {
-    const transformedIngredients = Object.keys(props.ingredients)
-    .map(igKey => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
             return [...Array(props.ingredients[igKey])]
-            .map((_, i) => {
-                return <BurgerIngredient key={igKey + i} type={igKey} />
-            })
+                .map((_, i) => {
+                    return <BurgerIngredient key={igKey + i} type={igKey} />
+                })
         }
-    )
+        ).reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
